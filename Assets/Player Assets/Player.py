8184d2ebadd0c4e16e38da67cdc7b9c2ab29d89a
RsDrftr <yusuf.fesan@gmail.com>
2022-05-17 23:57:26 -0400

    V 1.0

--- Assets/Player Assets/Player.py
+++ Assets/Player Assets/Player.py
@@ -1,6 +1,13 @@
+from typing import Iterable
 import pygame
 
 class Player:
 
-    def __init__(self):
-        
+    def __init__(self, posIn: list, surface, surface_dimensions: list, speed):
+        self.posX = posIn[0]
+        self.posY = posIn[1]
+        self.main_surface = surface
+        self.surfaceX = surface_dimensions[0]
+        self.surfaceY = surface_dimensions[1]
+
+        self.speed = speed
--- Gravity Dood.py
+++ Gravity Dood.py
@@ -14,3 +14,47 @@
 #   ...
 #-----------------------------------------------------------------------------
 
+import pygame
+
+def main():
+    #-----------------------------Setup------------------------------------------------------#
+    """ Set up the game and run the main game loop """
+    pygame.init()      # Prepare the pygame module for use
+    surfaceSize = [750, 350]   # Desired physical surface size, in pixels.
+    
+    clock = pygame.time.Clock()  #Force frame rate to be slower
+
+    # Create surface of (width, height), and its window.
+    mainSurface = pygame.display.set_mode((surfaceSize[0], surfaceSize[1]))
+
+    #-----------------------------Program Variable Initialization----------------------------#
+    # Set up some data to describe a small circle and its color
+
+
+
+    #-----------------------------Main Program Loop---------------------------------------------#
+    while True:       
+        #-----------------------------Event Handling-----------------------------------------#
+        ev = pygame.event.poll()    # Look for any event
+        if ev.type == pygame.QUIT:  # Window close button clicked?
+            break                   #   ... leave game loop
+
+
+        #-----------------------------Program Logic---------------------------------------------#
+        # Update your game objects and data structures here...
+
+
+        #-----------------------------Drawing Everything-------------------------------------#
+        # We draw everything from scratch on each frame.
+        # So first fill everything with the background color
+        mainSurface.fill((0, 200, 255))
+
+        # Now the surface is ready, tell pygame to display it!
+        pygame.display.flip()
+        
+        clock.tick(60) #Force frame rate to be slower
+
+
+    pygame.quit()     # Once we leave the loop, close the window.
+
+main()
