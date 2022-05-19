4f60f0e12540115e3b5bd3ac7508516949f58ee0
RsDrftr <yusuf.fesan@gmail.com>
2022-05-19 14:43:28 -0400

    Update to Ver 1
    I have decided on a spawning algorithm for the obstacle spawning inspired by jetpack joyride. For it I will be using a .json file to assign a limit and range which the obstacles can spawn from

--- Assets/Player Assets/Player.py
+++ Assets/Player Assets/Player.py
@@ -1,5 +1,5 @@
-from typing import Iterable
 import pygame
+import os
 
 class Player:
 
@@ -10,4 +10,25 @@ class Player:
         self.surfaceX = surface_dimensions[0]
         self.surfaceY = surface_dimensions[1]
 
+        self.animations = pygame.image.load(os.path.join("Player_Assets/Animations", "adventurer-v1.5-Sheet.png"))
+
+        self.shell = [54, 42, 15, 33]
+                     # x, y, width, height
         self.speed = speed
+
+    def gravity(self):
+
+
+    def player_run(self):
+        shell_phase = 0
+        max_phase = 6
+
+        if shell_phase < max_phase - 1:
+            shell_phase += 1
+            self.shell[0] += self.shell[2]
+
+        elif shell_phase >= max_phase:
+            shell_phase = 0
+            self.shell[0] -= self.shell[2] * (max_phase - 1)
+        
+        self.main_surface.blit(self.animations, (self.surfaceX // 2, self.surfaceY // 2), self.shell)
