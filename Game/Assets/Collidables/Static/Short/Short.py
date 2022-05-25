b62881a1aef38ad15a1ba9f37f0e60d512be8bf5
RsDrftr <yusuf.fesan@gmail.com>
2022-05-24 23:45:53 -0400

    Used abstract classes to help with trap spawning integration

--- Game/Assets/Collidables/Static/Short/Short.py
+++ Game/Assets/Collidables/Static/Short/Short.py
@@ -1,17 +1,51 @@
 import os
 import pygame
+from abc import ABC
 
 
-class Short:
+class Short(ABC):
 
-    def __init__(self, posIn):
-        self.posX = posIn[0]
-        self.posY = posIn[1]
+    def __init__(self, surface):
+        self.main_surface = surface
         
         self.spike_rate = 7
         self.saw_rate = 7
         self.acid_rate = 7
 
-        self.saw = pygame.image.load(os.path.join("Game\Assets\Collidables\Static\Short", "Saw Trap - Level 1.png"))
-        self.spike = pygame.image.load(os.path.join("Game\Assets\Collidables\Static\Short", "Spike_B.png"))
-        self.acid = pygame.image.load(os.path.join("Game\Assets\Collidables\Static\Short", "Toxic Trap - Level 1.png"))
+        self.animations = [pygame.image.load(os.path.join("Game\Assets\Collidables\Static\Short", "Saw Trap - Level 1.png")),   # Saw Spritesheet
+                           pygame.image.load(os.path.join("Game\Assets\Collidables\Static\Short", "Spike_B.png")),              # Spike Spritesheet
+                           pygame.image.load(os.path.join("Game\Assets\Collidables\Static\Short", "Toxic Trap - Level 1.png"))] # Acid Spritesheet
+
+    def draw_trap(self, animation):
+        self.main_surface.blit(self.animations[animation], self.main_surface, self.shell)
+
+class Saw(Short):
+
+    def __init__(self, posIn, surface):
+        super.__init__(surface)
+        self.posX = posIn[0]
+        self.posY = posIn[1]
+
+        self.shell
+
+        self.shell_phase = 0
+        self.max_phase = 9
+
+
+class Spike(Short):
+
+    def __init__(self, posIn, surface):
+        super.__init__(surface)
+        self.posX = posIn[0]
+        self.posY = posIn[1]
+
+        self.shell
+
+class Acid(Short):
+
+    def __init__(self, posIn, surface):
+        super.__init__(surface)
+        self.posX = posIn[0]
+        self.posY = posIn[1]
+
+        self.shell
